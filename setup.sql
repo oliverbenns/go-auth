913b17dc362dfcfe53c2de970b310271308e05bf
Oliver Benns <oliverbenns@gmail.com>
2019-11-13 17:28:26 +0800

    Add hashes

--- setup.sql
+++ setup.sql
@@ -6,6 +6,12 @@ CREATE TABLE users(
 );
 
 INSERT INTO users(email, hash)
-VALUES('john@example.com', 'first_hash'),
-('adam@example.com', 'second_hash'),
-('sophie@example.com', 'third_hash');
+VALUES
+('john@example.com', '$2a$10$fEe7QWpxFF6ILvSEu/R8zObDNWx1c7vcMhbsjCqjJ1WcoG4ATN3A6'),
+('adam@example.com', '$2a$10$vUVJXgzZCAqog4rIuSKLBeiBiUzdLfkq1ikgT3POHRMplLmkR/1rK'), 
+('sophie@example.com', '$2a$10$l3Lm6n/GIm9.j8/DTe05seV8E/uUPsh3Ie4NK08ncVUxLKRCnFqcK');
+
+-- Passwords:
+-- John: 123456
+-- Adam: password
+-- Sophie: qwerty 
