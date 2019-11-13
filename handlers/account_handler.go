d9fbcb5f501671d08c07b86415df86ddeaca6ceb
Oliver Benns <oliverbenns@gmail.com>
2019-11-13 20:50:44 +0800

    Move handlers

---
+++ handlers/account_handler.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "net/http"
+
+func AccountHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
--- main.go
+++ main.go
@@ -3,33 +3,18 @@ package main
 import (
 	"fmt"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/oliverbenns/go-auth/handlers"
 	"net/http"
 	"os"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-}
-
-func signUpHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-}
-
-func accountHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-}
-
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/account", accountHandler)
-	mux.HandleFunc("/login", loginHandler)
-	mux.HandleFunc("/sign-up", signUpHandler)
-	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/account", handlers.AccountHandler)
+	mux.HandleFunc("/login", handlers.LoginHandler)
+	mux.HandleFunc("/sign-up", handlers.SignUpHandler)
+	mux.HandleFunc("/", handlers.IndexHandler)
 
 	port := os.Getenv("PORT")
 
