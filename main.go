aed6e4f4f92f09d2ad16a6b708d4dc04228dbca5
Oliver Benns <oliverbenns@gmail.com>
2019-11-13 16:30:28 +0800

    Add server foundation

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func accountHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/account", accountHandler)
+	mux.HandleFunc("/login", loginHandler)
+	mux.HandleFunc("/", indexHandler)
+	fmt.Println("Server started on port", port)
+	http.ListenAndServe(":"+port, mux)
+}
