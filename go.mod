daaffeae08c69838de06a87605fa01e2e11393fc
Oliver Benns <oliverbenns@gmail.com>
2019-11-14 16:25:56 +0800

    Add basic credential check

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lib/pq v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20191112222119-e1110fd1c708
 )
--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -1,7 +1,74 @@
 package handlers
 
-import "net/http"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseFiles("views/layout.tmpl", "views/login.tmpl"))
+
+type Alert struct {
+	Message string
+	Theme   string
+}
+
+func setUserToken(w http.ResponseWriter, userToken string) {
+	cookie := http.Cookie{
+		Name:     "user_token",
+		Value:    userToken,
+		Domain:   "localhost",
+		Path:     "/",
+		MaxAge:   0,
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, &cookie)
+}
+
+func isAuthenticatedUser(r *http.Request) bool {
+	cookie, err := r.Cookie("user_token")
+
+	if err != nil {
+		return false
+	}
+
+	// @TODO: validate jwt.
+	validUser := cookie.Value == "abc"
+
+	return validUser
+}
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	if r.Method == "GET" {
+		if isAuthenticatedUser(r) {
+			http.Redirect(w, r, "/account", http.StatusFound)
+		} else {
+			tmpl.Execute(w, nil)
+		}
+	} else if r.Method == "POST" {
+		// email := r.FormValue("email")
+		password := r.FormValue("password")
+		// r.
+		// @TODO: lookup user in DB
+		// qwerty
+		hash := "$2a$10$l3Lm6n/GIm9.j8/DTe05seV8E/uUPsh3Ie4NK08ncVUxLKRCnFqcK"
+		err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+		validCredentials := err == nil
+
+		if validCredentials {
+			// @TODO issue jwt
+			jwt := "abc"
+			setUserToken(w, jwt)
+			alert := Alert{"Success!", "success"}
+			tmpl.Execute(w, alert)
+			// @TODO: Redirect
+			// http.Redirect(w, r, "/account", http.StatusFound)
+		} else {
+			alert := Alert{"Invalid credentials. Please try again.", "danger"}
+			tmpl.Execute(w, alert)
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
