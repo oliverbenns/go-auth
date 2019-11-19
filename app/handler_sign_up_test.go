5b816cf141a5e54b624858d3e2762b03dac52e99
Oliver Benns <oliverbenns@gmail.com>
2019-11-19 14:41:50 +0800

    Add get tests for login and signup

---
+++ app/handler_sign_up_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSignUpGetHandler_NoUser(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	server := Server{}
+
+	handler := server.SignUpHandler()
+	handler(w, r)
+
+	if w.Code != http.StatusOK {
+		t.Error("Does not respond with correct status")
+	}
+}
+
+func TestSignUpGetHandler_WithUser(t *testing.T) {
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV4YW1wbGVAZXhhbXBsZS5jb20iLCJpZCI6Mn0.fsA-0yhLc_XwndToIxmytRkBmvD78akk1mkJ7Be_xNs"
+
+	cookie := http.Cookie{
+		Name:  "user_token",
+		Value: token,
+	}
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	r.AddCookie(&cookie)
+	server := Server{}
+	server.env.JwtSecretKey = "12345"
+
+	handler := server.SignUpHandler()
+	handler(w, r)
+
+	if w.Code != http.StatusFound {
+		t.Error("Does not redirect")
+	}
+}
